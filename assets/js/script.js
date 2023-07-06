8a28ace77567df5c6e340af27a2cfcf7981540f8
sibekov <>
2023-07-06 23:27:57 +0000

    Add random operand selector

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ console.log("Ijava Iyahlupha jerrr!!!");
 
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
-
     for (let button of buttons) {
         button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
@@ -11,22 +10,36 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             else {
                 let mygameType = this.getAttribute("data-type");
-                alert(`You clicked ${mygameType}`);
+                myrunGame(mygameType);
             }
         });
     }
 
+    myrunGame("Jesus");
 });
 
-function myrunGame() {
+function myrunGame(mygameType) {
 
+    let charactor = "Jesus";
+
+    let operand = ["water?", "a rock?", "a tree?", "a fish?", "fire?"][Math.floor(10 * Math.random() / 2)];
+    if (mygameType === "Jesus") {
+        displayjesusQuestion(charactor, operand);
+    } else {
+        alert(`unknown game type:${mygameType}`);
+        throw `unknown game type:${mygameType}. Aborting!!!`;
+    }
 }
+
+
 
 function mycheckAnswer() {
 
 }
 
-function myworkedoutAnswer() {
+function myworkedoutAnswer(charactor, operand) {
+    document.getElementById('mycharactor').textContent = charactor;
+    document.getElementById('myoperand').textContent = operand;
 
 }
 
