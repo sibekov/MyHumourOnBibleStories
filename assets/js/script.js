3f558b95c4307397d5a20059d84b9cf39c33482a
sibekov <>
2023-07-06 19:50:31 +0000

    Add fuctions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,44 @@
-console.log(
-    "My Printa!!!!!"
-);
 
 
 //https://www.w3schools.com/tags/av_prop_playbackrate.asp
 
 let vid = document.getElementById("myvideo");
 vid.playbackRate = 0.05;
+
+
+function myrunGame() {
+
+}
+
+function mycheckAnswer() {
+
+}
+
+function myworkedoutAnswer() {
+
+}
+
+function myincreaseScore() {
+
+}
+
+function myincreaselossAnswer() {
+
+}
+
+
+function displayjesusQuestion() {
+
+}
+
+function displayjonaQuestion() {
+
+}
+
+function displyelijahQuestion() {
+
+}
+
+function displaymosesQuestion() {
+
+}
